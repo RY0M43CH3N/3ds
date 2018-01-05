2678bf6b3b4fc3abb7a122033dc048357818f94c
xKyu <antherynx.studio@mail.com>
2018-01-04 22:16:39 -0500

    LMAO

--- web/create-community.php
+++ web/create-community.php
@@ -17,6 +17,8 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+exit; // DISABLED BECUS ME ANGERY >:(
+
 if (getenv("CREATION_PASSWORD") != $_GET["password"]) {
 	die("Invalid password");
 }
