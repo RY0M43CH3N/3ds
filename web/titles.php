01225c9fa07a314a4909ef48fed11da132007d61
xKyu <antherynx.studio@mail.com>
2018-01-12 04:56:50 -0500

    okay

--- web/titles.php
+++ web/titles.php
@@ -27,7 +27,7 @@ $twig = $core->initTwig();
 
 $mysqli = $database->connect();
 
-$stmt = $mysqli->prepare("SELECT * FROM `communities` WHERE id = ?");
+$stmt = $mysqli->prepare("SELECT * FROM communities WHERE id = ? LIMIT 1");
 if (!$stmt):
 	error_log($mysqli->error);
 	die($mysqli->error);
