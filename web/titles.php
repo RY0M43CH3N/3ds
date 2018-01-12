3dd8a15c630f107844de0f23a73f57fd90324d0f
xKyu <antherynx.studio@mail.com>
2018-01-12 04:29:13 -0500

    test

--- web/titles.php
+++ web/titles.php
@@ -33,7 +33,7 @@ if (!$stmt):
 	die($mysqli->error);
 endif;
 
-$stmt->bind_param("i", abs($id));
+$stmt->bind_param("i", -215499880997580768);
 if (!$stmt->execute()) {
 	error_log("Failed to execute $stmt - " . $stmt->error);
 	die("Failed to execute $stmt");
