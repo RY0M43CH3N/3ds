683958e5cb607409e24bfff7b36b57e2542c0d27
xKyu <antherynx.studio@mail.com>
2018-01-12 04:48:05 -0500

    fatass1

--- web/titles.php
+++ web/titles.php
@@ -33,7 +33,7 @@ if (!$stmt):
 	die($mysqli->error);
 endif;
 $id = 1;
-$stmt->bind_param("i", $id = 1);
+$stmt->bind_param("i", $id);
 if (!$stmt->execute()) {
 	error_log("Failed to execute $stmt - " . $stmt->error);
 	die("Failed to execute $stmt");
