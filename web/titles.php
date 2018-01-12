398da9e24bf5f292e0663175fa2cfbe859895c3b
xKyu <antherynx.studio@mail.com>
2018-01-12 04:23:41 -0500

    this probably isn't going to work

---
+++ web/titles.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * This file is part of foxverse
+ * Copyright (C) 2017 Steph Lockhomes, Billy Humphreys
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+require_once("../lib/Core.php");
+require_once("../lib/Database.php");
+session_start();
+
+$core = new Core();
+$database = new Database();
+$twig = $core->initTwig();
+
+$mysqli = $database->connect();
+
+$stmt = $mysqli->prepare("SELECT * FROM `communities` WHERE id = ? LIMIT 1");
+if (!$stmt):
+	error_log($mysqli->error);
+	die($mysqli->error);
+endif;
+
+$stmt->bind_param("i", $id);
+if (!$stmt->execute()) {
+	error_log("Failed to execute $stmt - " . $stmt->error);
+	die("Failed to execute $stmt");
+}
+
+echo $twig->render("titles.twig", ["community" => $database->getResult($stmt)]);
