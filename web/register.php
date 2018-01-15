4f61679600277648ba09bf4d43a0e38972281d03
ninjafox <antherynx.studio@mail.com>
2018-01-15 12:16:23 -0500

    hi my name is billy mays

--- web/register.php
+++ web/register.php
@@ -46,11 +46,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 	echo(" preparing1");
 
 	$stmt = $mysqli->prepare("INSERT INTO `users` (`user_pid`, `user_ip`, `user_display_name`, `user_username`, `user_password`, `user_nnid`, `user_email`, `user_country_id`, `user_systems_owned`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-	if (!$stmt):
+	/*if (!$stmt):
 		echo("error");
 		error_log($mysqli->error);
 		die($mysqli->error);
-	endif;
+	endif;*/
 	echo(" preparing");
 
 	$systems_owned = 1;
