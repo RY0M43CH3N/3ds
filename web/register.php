b3e49b7bc5c4b83ae93b011497d204736ea85abf
ninjafox <antherynx.studio@mail.com>
2018-01-15 22:29:27 -0500

    Add verification

--- web/register.php
+++ web/register.php
@@ -25,13 +25,54 @@ $core = new Core();
 $database = new Database();
 $mysqli = $database->connect();
 
-function registerError($str) {
+function errorRedirect($str) {
 	header("Location: " . $_SERVER["REQUEST_URI"] . "?err=" . $str);
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-	//registerError("Registration is currently disabled.");
-	//exit;
+	if (isset($_POST["display_name"]) && !empty($_POST["display_name"])) {
+		if (strlen($_POST["display_name"]) < 6 || strlen($_POST["display_name"]) > 24) {
+			errorRedirect("Display Name length must be between 6-24.");
+			exit;
+		}
+	} else {
+		errorRedirect("Please enter a display name.");
+		exit;
+	}
+
+	if (isset($_POST["username"]) && !empty($_POST["username"])) {
+		if (strlen($_POST["username"]) < 6 || strlen($_POST["username"]) > 16) {
+			errorRedirect("Username length must be between 6-16.");
+			exit;
+		}
+	} else {
+		errorRedirect("Please enter a username.");
+		exit;
+	}
+
+	if (isset($_POST["email"]) && !empty($_POST["email"])) {
+		if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+			errorRedirect("Invalid email, please enter a valid email.");
+			exit;
+		}
+	} else {
+		errorRedirect("Please enter a email.");
+		exit;
+	}
+
+	$nnid = null;
+
+	if (isset($_POST["nnid"]) && !empty($_POST["email"])) {
+		if (strlen($_POST["nnid"]) < 6 || strlen($_POST["nnid"]) > 16) {
+			errorRedirect("Nintendo Network ID length must be between 6-16.");
+			exit;
+		}
+
+		$nnid = $_POST["nnid"];
+	} else {
+		errorRedirect("Please enter a Nintendo Network ID.");
+		exit;
+	}
 	echo $twig->render("registerSuccess.twig");
 	exit;
 }
