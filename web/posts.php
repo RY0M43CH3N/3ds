bb03b2e15a6c2b50e00c60af0245632bcf559d93
ninjafox <antherynx.studio@mail.com>
2018-01-21 18:45:50 -0500

    fix??

--- web/posts.php
+++ web/posts.php
@@ -37,7 +37,7 @@ if ($_SESSION["user"]) {
 		}
 
 		$community = $database->getResult($stmt)[0];
-		if ($_SESSION["user"]["user_permission"] > $community["community_permission"] || $community["community_permission"] == 0) {
+		if ($_SESSION["user"]["user_permission"] > $community["community_permission"] - 1 || $community["community_permission"] == 0) {
 		} else {
 			exit;
 		}
